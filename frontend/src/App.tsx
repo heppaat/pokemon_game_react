fc6a1f7696acdd0b06afec9128e65c6c867f8206
heppaat <pali.herrer@gmail.com>
2024-05-11 14:25:47 +0200

    tailwind on all locations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { getLocations, getSingleLocation } from "./api";
 import Encounter from "./components/Encounter";
 import logo from "./assets/logo.png";
 import pikachu from "./assets/pikachu.png";
+import { GradientColor } from "./enums";
 
 const App = () => {
   const [startGame, setStartGame] = useState<boolean>(false);
@@ -61,11 +62,15 @@ const App = () => {
           </section>
           <div className="flex justify-center gap-6 p-4">
             <section className="max-w-[300px] relative">
-              <img src={pikachu} alt="pikachu" />
+              <img
+                className="hover:scale-105 transition-scale duration-300 ease-in-out hover:rotate-2"
+                src={pikachu}
+                alt="pikachu"
+              />
             </section>
             <section className="max-w-[250px] w-[200px] relative">
               <button
-                className=" bg-[#3b4cca] text-[#fbfff4] font-bold uppercase px-8 py-4 rounded-full absolute bottom-10 hover:scale-105 hover:opacity-90 hover:shadow-xl transition-opacity, transition-scale duration-300 ease-in-out"
+                className="bg-[#3b4cca] text-[#fbfff4] font-bold uppercase px-8 py-4 rounded-full absolute bottom-10 hover:scale-105 hover:bg-[#475bf3] hover:shadow-xl transition-scale duration-300 ease-in-out"
                 onClick={handleGetLocations}
               >
                 Start Game
@@ -76,18 +81,47 @@ const App = () => {
       )}
 
       {startGame && locations && !singleLocation && (
-        <div>
-          {locations.results.map((location, index) => (
-            <div key={index}>
-              <h2>{location.name}</h2>
-              <p>{location.url}</p>
-              <button onClick={() => handleGetSingleLocation(location.url)}>
-                Select
+        <main>
+          <section className="flex justify-center">
+            <div
+              className={`flex justify-center p-10 font-extrabold text-[#fbfff4] text-[58px] w-[800px] m-10 rounded-3xl bg-gradient-to-t ${GradientColor.blueToBlue} shadow-xl`}
+            >
+              <h1 className="text-center">CHOOSE YOUR LOCATION</h1>
+            </div>
+          </section>
+          <div className="flex flex-wrap gap-8 p-8 justify-center">
+            {locations.results.map((location, index) => (
+              <div
+                key={index}
+                className={`flex flex-col justify-center items-center gap-5 w-[300px] h-[150px] rounded-2xl bg-gradient-to-b ${GradientColor.yellowToYellow}  hover:scale-105 hover:shadow-md duration-300 ease-in-out`}
+              >
+                <h2 className="uppercase font-semibold text-center px-4">
+                  {location.name}
+                </h2>
+                <div
+                  className={`bg-gradient-to-t ${GradientColor.blueToBlue} px-4 py-2 rounded-xl text-[#fbfff4]`}
+                >
+                  <button
+                    className="uppercase font-medium"
+                    onClick={() => handleGetSingleLocation(location.url)}
+                  >
+                    Select
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex justify-center m-10 pb-40">
+            <div className="bg-[#C52018] rounded-full px-8 py-4 hover:scale-105 hover:bg-[#eb271c] hover:shadow-xl transition-scale duration-300 ease-in-out">
+              <button
+                className="text-[25px] text-[#fbfff4] uppercase font-semibold"
+                onClick={handleBackToMainPage}
+              >
+                Back to main page
               </button>
             </div>
-          ))}
-          <button onClick={handleBackToMainPage}>Back to main page</button>
-        </div>
+          </div>
+        </main>
       )}
 
       {singleLocation && randomPokemon && (
